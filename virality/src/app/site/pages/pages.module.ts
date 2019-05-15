c1382e2722b5c6cb14812c0a32a41af7ebcd1651
Dat Nguyen <dat1nguyen1@gmail.com>
2019-05-15 15:58:41 -0700

    fix links

--- virality/src/app/site/pages/pages.module.ts
+++ virality/src/app/site/pages/pages.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, Router } from "@angular/router";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SharedModule } from "../../shared/shared.module";
 
-import { AboutComponent } from './about/about.component';
+import { GroupsComponent } from './groups/groups.component';
 import { FaqComponent } from './faq/faq.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
+  { path: 'groups', component: GroupsComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'contact', component: ContactComponent }
 ]
 
 @NgModule({
-  declarations: [AboutComponent, FaqComponent, ContactComponent],
+  declarations: [GroupsComponent, FaqComponent, ContactComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule.forChild(routes),
     SharedModule
   ], exports: [
